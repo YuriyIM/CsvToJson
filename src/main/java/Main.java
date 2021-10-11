d558e8b8a416d1534afc94c46a3ee39ae7465509
YuriyIM <vehpby@gmail.com>
2021-10-11 07:43:40 +0200

    try add xml parser.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,68 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) throws ParserConfigurationException, IOException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
 
         List<Employee> list = parseCSV(columnMapping, fileName);
-
         String json = listToJson(list);
-        writeString(json);
+        writeString(json, "data.json");
+
+//        List<Employee> list2 = parseXML("data.xml");
+//        String json2 = listToJson(list2);
+//        writeString(json2, "data2.json");
+
     }
 
-    private static void writeString(String json) {
-        try (FileWriter file = new FileWriter("data.json")) {
+//    private static List<Employee> parseXML(String s) throws ParserConfigurationException, IOException, SAXException {
+//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder builder = factory.newDocumentBuilder();
+//        Document doc = builder.parse(new File(s));
+//        Node root = doc.getDocumentElement();
+//        String[] employeeString = read(root);
+//        System.out.println(employeeString);
+//        List<Employee> employees = new ArrayList<>();
+//        return employees;
+//    }
+
+//    private static String[] read(Node node) {
+//        NodeList nodeList = node.getChildNodes();
+//        for (int i = 0; i < nodeList.getLength(); i++) {
+//            Node node_ = nodeList.item(i);
+//            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+//                Element element = (Element) node_;
+//                NamedNodeMap map = element.getAttributes();
+//                for (int a = 0; a < map.getLength(); a++) {
+//                    String attrName = map.item(a).getNodeName();
+//                    String attrValue = map.item(a).getNodeValue();
+//                }
+//                read(node_);
+//            }
+//        }
+//    }
+
+    private static void writeString(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
@@ -31,9 +71,10 @@ public class Main {
     }
 
     private static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        return gson.toJson(list);
+        return gson.toJson(list, listType);
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
